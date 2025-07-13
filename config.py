bf88ab87253bc55dcdffc3ef538f6c658c8d44e0
Andreas Domberg <andom@posteo.de>
2025-07-13 13:00:21 +0200

    set to KF as example source

--- config.py
+++ config.py
@@ -6,7 +6,12 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-PROJECT_NAME = "Decoding"
+PROJECT_NAME = "Knowledge Fight"
+SOURCE = "http://feeds.libsyn.com/92106/rss"
+
+#
+# Settings about the transcription and rendering APIs
+#
 
 # Renderer:
 RENDERER_CONFIG = {
@@ -17,15 +22,14 @@ RENDERER_CONFIG = {
     # 10000 - 9 frames per hour (for testing)
     "frame_step": 100,
 }
+
 # Connector:
-# Specify class paths as strings. Open to adding more connectors and transcribers.
 CONNECTOR_CLASS = "podology.data.connectors.rss.RSSConnector"
-CONNECTOR_ARGS = {
-    "remote_resource": "https://decoding-the-gurus.captivate.fm/rssfeed"
-}
+CONNECTOR_ARGS = {"remote_resource": SOURCE}
+
 # Transcriber:
-# DA is for testing: no audio files are downloaded.
-# If True, set the transcriber endpoint to "dummytranscribe" below.
+# Dummy audio is for testing: no audio files are downloaded. If True and using
+# the WhisperX transcriber API, its endpoint should be set to "dummytranscribe" below.
 DUMMY_AUDIO = False
 TRANSCRIBER_CLASS = "podology.data.transcribers.whisperx.WhisperXTranscriber"
 TRANSCRIBER_ARGS = {
@@ -37,16 +41,18 @@ TRANSCRIBER_ARGS = {
 # Stopwords concern the identification of named entities in scroll video rendering
 # and stats. They are still transcribed, and still found by Elasticsearch.
 PROJECT_STOPWORDS = [
-    i.lower() for i in [
-        # "Chris",  # e.g., presenter names
-        # "Matt",
+    i.lower()
+    for i in [
+        # "Dan",  # e.g., presenter names
+        # "Jordan",
         # ...
     ]
 ]
 
 # Set of stopwords that don't vary from pod to pod:
 STOPWORDS = [
-    i.lower() for i in [
+    i.lower()
+    for i in [
         "okay",
         "fair",
         "which",
@@ -56,8 +62,9 @@ STOPWORDS = [
         "right",
         "yep",
         "same",
-        "wait"
-    ] + PROJECT_STOPWORDS
+        "wait",
+    ]
+    + PROJECT_STOPWORDS
 ]
 
 # -----------------------------------------------------------------------------
@@ -81,14 +88,17 @@ TRANSCRIPT_DIR.mkdir(parents=True, exist_ok=True)
 WORDCLOUD_DIR.mkdir(parents=True, exist_ok=True)
 DB_PATH.parent.mkdir(parents=True, exist_ok=True)
 
+
 def get_class(class_path):
     module_name, class_name = class_path.rsplit(".", 1)
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
 
+
 def get_connector():
     cls = get_class(CONNECTOR_CLASS)
     return cls(**CONNECTOR_ARGS)
+
 
 def get_transcriber():
     cls = get_class(TRANSCRIBER_CLASS)
